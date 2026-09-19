ef4b404307d96590aa55f56fe85ebbcc5707fc9a
lizyyy <lizyyy@users.noreply.github.com>
2026-09-19 23:05:03 +0900

    Initial environment snapshot (ZY10057)

---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+
+VERSION=$1
+
+git cliff --unreleased --tag $VERSION --prepend CHANGELOG.md
+
+git add CHANGELOG.md
+git commit -m "Updated changelog"
+cargo release --execute $VERSION
